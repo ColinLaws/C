d6c7eb00816e5d163bcabc95d403d3fe383eb031
Colin Laws <colinalaws@gmail.com>
2024-10-14 16:46:15 -0400

    Rewrote the fahrenheit to celsius program to use a function for the conversion logic

--- fahr2cels/main.c
+++ fahr2cels/main.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
 
-/* print a Fahrenheit-Celsius table 
-    for fahr = 0, 20, ..., 300 */
-// int main()
-// {
-//     float fahr, celsius;
-//     int lower, upper, step;
-
-//     lower = 0;      /* lower limit of the temp table */
-//     upper = 300;    /* upper limit of the temp table */
-//     step = 20;      /* step size */
-
-//     fahr = lower;   /* start the table at the lower limit */
-
-//     /* table header */
-//     printf("Fahr\tCels\n");
-//     printf("-----------------------\n");
-
-//     while (fahr <= upper) {
-//         celsius = (5.0/9.0) * (fahr - 32.0);
-//         printf("%3.0f\t\t%3.2f\n", fahr, celsius);
-//         fahr += step;
-//     }
-// }
-
 #define     LOWER   0
 #define     UPPER   300
 #define     STEP    20
